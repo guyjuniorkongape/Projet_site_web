c70d158d99b23bd1546cd0bb867fb45a9148656c
MerveilleKamga <merveille.kamga@groupe-esigelec.org>
2025-11-01 15:16:35 +0100

    Création2

--- index.php
+++ index.php
@@ -1,12 +1,24 @@
 <!DOCTYPE html>
 <html lang="fr">
 <head>
-    <meta charset="UTF-8">
-    <title>Bonjour</title>
+  <meta charset="UTF-8">
+  <title>ESIG'MOVING</title>
+  <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <?php
-        echo "<h1>Bonjour !</h1>";
-    ?>
+  <div class="container">
+    <div class="center-panel">
+      <h1>EM</h1>
+      <h1>ESIG'MOVING</h1>
+      <div class="buttons">
+        <button>CRÉER UN COMPTE</button>
+        <button>CONNEXION</button>
+        <button>INFORMATIONS</button>
+      </div>
+    </div>
+    <div class="right-panel">
+      <img src="image.jpg" alt="Personnes avec cartons">
+    </div>
+  </div>
 </body>
 </html>
