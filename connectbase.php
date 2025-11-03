e53b9d9ab389a061720051c748bc2dd51726e8a6
Merveille.Kamga <merveille.kamga@groupe-esigelec.org>
2025-11-03 12:20:15 +0100

    connectbase

---
+++ connectbase.php
@@ -0,0 +1,16 @@
+<?php
+$servername = 'localhost';
+$username = 'root';
+$password = 'root';
+$dbname = 'grp_7_3';
+try {
+   // Création de la connexion PDO
+   $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+   // Configuration du mode d'erreur PDO sur Exception
+   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+   echo "Connexion réussie";
+} catch (PDOException $e) {
+   // Gestion des erreurs
+   echo "Erreur de connexion : " . $e->getMessage();
+}
+?>
